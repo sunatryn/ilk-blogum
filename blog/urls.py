fb4c18ea4139fc0595bb16503e342057048410cf
suna <tariyansuna@gmail.com>
2016-04-04 22:47:06 +0300

    Site için yeni template ve view ekledim.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 # Django'ya eğer siteye biri 'http://127.0.0.1:8000/' adresinden gelirse
 # gitmesi gereken yerin views.post_list olduğunu söylüyor.
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
